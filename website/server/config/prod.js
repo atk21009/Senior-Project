ab555bec5896f85c35291251e2ca0547dc129961
Taylor Atkin <104331577+atk21009@users.noreply.github.com>
2023-10-09 06:08:14 -0600

    Delete website/server/config/prod.js

--- website/server/config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://atk21009:M76WlTbRCf4Dwlzv@dbs-dev.qzmenne.mongodb.net/",
-  cookieKey: "rzd;753-'6H#!($4@mZ:9^{wJ+,?SebMu}L[Y.*q",
-  accessTokenSecret: "",
-};

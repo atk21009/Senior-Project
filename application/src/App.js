59163534fff6f18c2c7a66b2dda50abd84525335
Taylor <atk21009@byui.edu>
2023-10-30 05:09:18 -0600

    Update package and app

--- application/src/App.js
+++ application/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Editt <code>src/App.js</code> and save to reload.
+          Edittt <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

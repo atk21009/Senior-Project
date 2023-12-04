6c37b70aa893924e95801b64c7b13a00fc4a4ac5
Taylor <atk21009@byui.edu>
2023-12-04 08:42:02 -0700

    Update Header.js

--- website/client/src/components/Header.js
+++ website/client/src/components/Header.js
@@ -35,7 +35,9 @@ class Headercontent extends Component {
           <Link to="/">Home</Link>
           <Link to="/features">Features</Link>
           <Link to="/pricing">Pricing</Link>
-          <Link to="/download">Download</Link>
+          <a href="https://github.com/atk21009/Senior-Project/releases/download/v1.0.0/ElectronReact.Setup.4.6.0.exe">
+            Download
+          </a>
           <Link to="/support">Support</Link>
         </div>
         <div className="header-auth">{this.renderContent()}</div>
